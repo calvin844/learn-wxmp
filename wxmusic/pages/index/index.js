63c3615dd88c2fd5f046267fbe2104676d210a41
calvin844 <377128799@qq.com>
2021-08-30 23:46:44 +0800

    20210830

---
+++ wxmusic/pages/index/index.js
@@ -0,0 +1,94 @@
+// pages/index/index.js
+import request from '../../utils/request'
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        bannerList: [], //轮播图数据
+        recommendList: [], //推荐歌单数据
+        topList: [], //排行榜数据
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: async function (options) {
+        let bannerListData = await request('/banner', {
+            type: 2
+        });
+        let recommendListData = await request('/personalized', {
+            limit: 10
+        });
+        this.setData({
+            bannerList: bannerListData.banners,
+            recommendList: recommendListData.result
+        })
+        let index = 0;
+        let topListArr = []
+        while (index < 5) {
+            let topListData = await request('/top/list', {
+                idx: index++
+            });
+            let topListItem = {
+                name: topListData.playlist.name,
+                tracks: topListData.playlist.tracks.slice(0, 3)
+            }
+            topListArr.push(topListItem)
+            // 不需要等待5次请求全部完成后才更新，但是会造成多次渲染，牺牲性能
+            this.setData({
+                topList: topListArr
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ wxmusic/utils/request.js
@@ -0,0 +1,42 @@
+// 发送ajax请求
+/**
+ * 1. 封装功能函数
+ *    1. 功能点明确
+ *    2. 函数内部应该保留固定代码
+ *    3. 将动态的数据抽取成形参，由使用者根据自身的情况动态的传入实参
+ *    4. 一个良好的功能函数应该设置形参的默认值
+ * 
+ * 2. 封装功能组件
+ *    1. 功能点明确
+ *    2. 组件内部保留静态的代码
+ *    3. 将动态的数据抽取成props参数，由使用者根据自身的情况以标签属性的形式动态传入props数据
+ *    4. 一个良好的组件应该设置组件的必要性及数据类型
+ *      props: {
+ *        msg: {
+ *          required: true,
+ *          default: 默认值,
+ *          type: String
+ *        }
+ *      }
+ */
+import config from './config'
+export default (url, data = {}, method = "GET") => {
+  return new Promise((resolve, reject) => {
+    // 1. new Promise初始化promise实例的状态为pending
+    wx.request({
+      url: config.host + url,
+      data,
+      method,
+      success: (res) => {
+        // resolve修改promise的状态为成功状态
+        resolve(res.data)
+      },
+      fail: (err) => {
+        // reject修改promise的状态为失败状态
+        reject(err)
+      }
+    })
+  })
+
+
+}
